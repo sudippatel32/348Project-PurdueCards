52d8061840492aaecd2ac1b55180fe7c57922824
Swapnil-Verma <verma119@purdue.edu>
2022-12-10 19:57:31 -0500

    Create monthly_report.sql
    Created the monthly report stored procedure

---
+++ Application/monthly_report.sql
@@ -0,0 +1,18 @@
+SET TRANSACTION ISLOATION LEVEL SERIALIZABLE;
+
+DELIMITER //
+
+CREATE PROCEDURE IF NOT EXISTS monthlyReport AS
+BEGIN
+  BEGIN TRANSACTION;
+    SELECT Sales.ID, Customer.name, Card.name, Sales.profit, Sales.quantity,
+     Sales.date
+    FROM Sales
+      JOIN Customer ON Sales.buyer_ID = Customer.ID
+      JOIN Card ON Sales.card_ID = Card.ID
+    WHERE DATEDIFF(month, Sales.date, CURRENT_DATE) <= 1
+    ORDER BY date DESC;
+  COMMIT;
+END
+//
+DELIMITER ;
